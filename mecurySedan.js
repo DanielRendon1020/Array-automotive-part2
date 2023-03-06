6b1beb98369ee359c121ca2323108fabd60dc577
DanielRendon1020 <d.danny1020@gmail.com>
2023-03-05 19:59:15 -0800

    completed class extension of VehicleModule class

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,40 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicle.js")
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+class mercurySedan extends VehicleModule{
+    constructor(make, model, year, color, mileage, maximumPassengers, passengers, numberOfWheels, maximumSpeed, fuel, scheduleService){
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = 5
+        this.passengers = 0
+        this.numberOfWheels = 4
+        this.maximumSpeed = 160
+        this.fuel = 10
+        this.scheduleService = false
+    }
+
+    loadPassenger(num){
+
+    }
+
+    start(){
+        if(this.fuel > 0){
+            return started === true
+        }
+        else{
+            return started === false
+        }
+    }
+
+    scheduleService(mileage){
+        if(mileage > 30000){
+            return timeForMaintenance === true
+        }
+        else{
+            return timeForMaintenance === false
+        }
+    }
+}
